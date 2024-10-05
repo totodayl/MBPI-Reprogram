c80c363c67e13595b1778b732c6b05cabc2211a4
totodayl <eroma.ronaldamiel@gmail.com>
2024-10-05 09:41:40 +0800

    fix query on getting the status_changed in QC

--- MBPI-System.py
+++ MBPI-System.py
@@ -6743,6 +6743,7 @@ class Ui_LoginWindow(object):
                 remarks = '{remarks_box.toPlainText()}', edited = true, updated_on = '{datetime.now()}'::timestamp,
 				status_changed = 
 				    CASE 
+				        WHEN (SELECT status_changed FROM quality_control WHERE lot_number = '{lot_number}') = 'true' THEN TRUE
                         WHEN (SELECT status FROM quality_control WHERE lot_number = '{lot_number}') != '{test_result_dropdown.currentText()}'
                         THEN true
                         else false
